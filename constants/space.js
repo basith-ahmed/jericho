f6eadd4dfd27aca018ee7b701c044f380dfa8b16
notkath <148864502+notkath@users.noreply.github.com>
2025-02-15 15:31:59 +0530

    data

--- constants/space.js
+++ constants/space.js
@@ -1,40 +1,65 @@
 const spaces = [
   {
-    title: "Space 1",
+    title: "Hubble's Star Clusters",
     href: "space-1",
     description: "Descripti",
-    image: "https://apod.nasa.gov/apod/image/2502/_D8A2600d_1024.jpg",
+    image: "https://science.nasa.gov/wp-content/uploads/2023/07/52515250436-6ea8fea1ca-o1.jpg?resize=1200,1126",
   },
   {
-    title: "Space 2",
+    title: "Phoenix Galaxy Cluster Forms Stars",
     href: "space-2",
     description: "Description 1",
     image: "https://live.staticflickr.com/65535/54324515037_119b0f39c6_b.jpg",
   },
   {
-    title: "Space 3",
+    title: "Hubble Ultra Deep Fields",
     href: "space-3",
     description: "Description 1",
-    image: "https://live.staticflickr.com/65535/54213487373_da73740475_b.jpg",
+    image: "https://live.staticflickr.com/65535/51218566742_1004d95078_z.jpg",
   },
   {
-    title: "Space 3",
+    title: "Hubble's Gravitational Lenses",
     href: "space-3",
     description: "Description 1",
-    image: "https://live.staticflickr.com/65535/52534406448_bc47b724f7.jpg",
+    image: "https://science.nasa.gov/wp-content/uploads/2023/04/stsci-h-p1958a-f-2300-jpg.webp?resize=1200,1072",
   },
   {
-    title: "Space 3",
+    title: "Hubble's Nebulae",
     href: "space-3",
     description: "Description 1",
-    image: "https://live.staticflickr.com/65535/52534406448_bc47b724f7.jpg",
+    image: "https://science.nasa.gov/wp-content/uploads/2023/06/hubble-trantula-potw2305a-jpg.webp?resize=1200,1189",
   },
   {
-    title: "Space 3",
+    title: "Hubble's Galaxies",
     href: "space-3",
     description: "Description 1",
-    image: "https://live.staticflickr.com/65535/52534406448_bc47b724f7.jpg",
+    image: "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/nebulae/emission/Hubble_LMC_TarantulaNebula_potw2506a.jpg?w=1863&h=2048&fit=crop&crop=faces%2Cfocalpoint",
   },
+  {
+    title: "Hubble's Galaxies",
+    href: "space-3",
+    description: "Description 1",
+    image: "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/nebulae/emission/Hubble_LMC_TarantulaNebula_potw2506a.jpg?w=1863&h=2048&fit=crop&crop=faces%2Cfocalpoint",
+  },
+  {
+    title: "Hubble's Galaxies",
+    href: "space-3",
+    description: "Description 1",
+    image: "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/nebulae/emission/Hubble_LMC_TarantulaNebula_potw2506a.jpg?w=1863&h=2048&fit=crop&crop=faces%2Cfocalpoint",
+  },
+  {
+    title: "Hubble's Galaxies",
+    href: "space-3",
+    description: "Description 1",
+    image: "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/nebulae/emission/Hubble_LMC_TarantulaNebula_potw2506a.jpg?w=1863&h=2048&fit=crop&crop=faces%2Cfocalpoint",
+  },
+  {
+    title: "Hubble's Galaxies",
+    href: "space-3",
+    description: "Description 1",
+    image: "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/nebulae/emission/Hubble_LMC_TarantulaNebula_potw2506a.jpg?w=1863&h=2048&fit=crop&crop=faces%2Cfocalpoint",
+  },
+
 ];
 
 
