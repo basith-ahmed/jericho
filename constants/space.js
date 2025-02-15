2d1f7c55402bbcd08ca6b1ab6cea61d5fdadfde2
Lassense <lakshmisanjeev1205@gmail.com>
2025-02-15 14:52:11 +0530

    discover-page

--- constants/space.js
+++ constants/space.js
@@ -2,17 +2,23 @@ const spaces = [
   {
     title: "Space 1",
     href: "space-1",
-    image: "/space-1.jpg",
+    image: "https://live.staticflickr.com/65535/52534406448_bc47b724f7.jpg",
+    description: `This is a description about the space. It provides insights, details, and interesting facts about the selected topic. 
+    The text is inside a translucent black box for better readability.`
   },
   {
     title: "Space 2",
     href: "space-2",
-    image: "/space-2.jpg",
+    image: "https://images-assets.nasa.gov/image/30dor/30dor~medium.jpg",
+    description: `This is a description about the space. It provides insights, details, and interesting facts about the selected topic. 
+    The text is inside a translucent black box for better readability.`
   },
   {
     title: "Space 3",
     href: "space-3",
-    image: "/space-3.jpg",
+    image: "https://images-assets.nasa.gov/image/PIA21073/PIA21073~medium.jpg",
+    description: `This is a description about the space. It provides insights, details, and interesting facts about the selected topic. 
+    The text is inside a translucent black box for better readability.`
   },
 ];
 
